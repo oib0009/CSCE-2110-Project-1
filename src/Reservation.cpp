7465474f029be1e10821394e9e0720a083c10f08
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-14 10:08:06 -0500

    Refactor Reservation constructor and validation method

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -1,39 +1,45 @@
 // Reservation implementation
 #include "../include/Reservation.h"
 
-Reservation::Reservation(){
-  reservationId = 0;
-  studentId = 0;
-  studentName = "";
-  resourceId = 0;
-  reservationDate = "";
-} //default const
-Reservation::Reservation(int reservationId, int studentId, string& studentName, int resourceId,
-            string& reservationDate){
-  this->reservationId = reservationId;
-  this->studentId = studentId;
-  this->studentName = studentName;
-  this->resourceId = resourceId;
-  this->reservationDate = reservationDate;
-  
-  } //const with reservation info
+// Default const
+Reservation::Reservation() {
+    reservationId = 0;
+    studentId = 0;
+    studentName = "";
+    resourceId = 0;
+    reservationDate = "";
+}
 
-//to check if the reservation data is valid
-bool Reservation::hasValiData() const{
-  if (reservation <= 0){
-    return false;
-  }
-  if(studentId <= 0){
-    return false;
-  }
-  if (studentName.empty()){
-    return false;
-  }
-  if (resourceId <= 0){
-    return false;
-  }
-  if (reservationDate.empty()){
-    return false
-  }
-  return true; 
-} 
+// Constructor with reservation information
+Reservation::Reservation(int reservationId,
+                         int studentId,
+                         const string& studentName,
+                         int resourceId,
+                         const string& reservationDate) {
+
+    this->reservationId = reservationId;
+    this->studentId = studentId;
+    this->studentName = studentName;
+    this->resourceId = resourceId;
+    this->reservationDate = reservationDate;
+}
+
+// Check if reservation data is valid
+bool Reservation::hasValidData() const {
+    if (reservationId <= 0) {
+        return false;
+    }
+    if (studentId <= 0) {
+        return false;
+    }
+    if (studentName.empty()) {
+        return false;
+    }
+    if (resourceId <= 0) {
+        return false;
+    }
+    if (reservationDate.empty()) {
+        return false;
+    }
+    return true;
+}
