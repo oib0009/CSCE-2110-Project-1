232849ea5746135f7c3075ecb5b0bf543bbc8618
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-14 09:52:56 -0500

    Add Reservation struct with constructors and validation method

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,21 @@
 // Reservation class
+#ifndef RESERVATION_H
+#define RESERVATION_H
+
+#include <string>
+using namespace std;
+
+struct Reservation {
+  int reservationId;
+  int studentId;
+  string studentName;
+  int resourceId;
+  string reservationDate;
+
+Reservation(); //default const
+Reservation(int reservationId, int studentId, string studentName, int resourceId,
+            string reservationDate); //const with reservation info
+bool hasValiData() const; //to check if the reservation data is valid
+
+};
+#endif
