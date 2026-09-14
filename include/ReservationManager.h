42e4373378cb80e006f1e046b1703436486d62d2
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-14 09:41:38 -0500

    Implement ReservationManager class
    Added ReservationManager class with methods for managing reservations.

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -1,1 +1,24 @@
 // Reservation manager and linked list
+#ifndef RESERVATION_MANAGER_H
+#define RESERVATION_MANAGER_H
+
+#include "Reservation.h"
+#include "linked_list.h"
+
+class ReservationManager{
+
+private:
+  ReservationNode* head;
+
+public:
+  ReservationManager(); //const
+  ~ReservationManager(); //destructor
+  bool addReservation(const Reservation& reservation); //create or add reservation
+  bool cancelReservation(int reservationId); //cancel or remove reservation
+  Reservation* findReservation(int reservationId); //find reservation by ID
+  void displayActiveReservation() const; //display all active reservation
+  bool validateReservation(const Reservation& reservation) const; //validate before adding it
+  bool reservationExists(int reservationId)const; //check if reservation exists
+  int getActiveReservationCount() const; //Number of active reservation
+};
+#endif
