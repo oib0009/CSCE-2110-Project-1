0b52c83cd387a39300da3ff11b8976c646f89d79
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-13 20:40:33 -0500

    Update Reservation.h

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,22 @@
 // Reservation class
+#ifndef RESERVATION_H
+#define RESERVATION_H
+
+/stores information of one reservaation
+#include <string>
+using namespace std;
+
+struct Reservation {
+  int reservationId;
+  int studentID;
+  string studentName;
+  int resourceId;
+  string reservationDate;
+
+Reservation(); //defult const
+Reservation(int reservationId, int studentID, string studentName, int resourceId, 
+            string reservationDate); //const with reservation infomation
+bool hasValidData() const; //to check is reservation data is valid
+
+};
+#endif
