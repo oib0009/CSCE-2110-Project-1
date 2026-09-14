3c620e60bd5259180734a3b538d57cc3917f4e3a
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-14 10:01:08 -0500

    Update Reservation.cpp

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -1,1 +1,39 @@
 // Reservation implementation
+#include "../include/Reservation.h"
+
+Reservation::Reservation(){
+  reservationId = 0;
+  studentId = 0;
+  studentName = "";
+  resourceId = 0;
+  reservationDate = "";
+} //default const
+Reservation::Reservation(int reservationId, int studentId, string& studentName, int resourceId,
+            string& reservationDate){
+  this->reservationId = reservationId;
+  this->studentId = studentId;
+  this->studentName = studentName;
+  this->resourceId = resourceId;
+  this->reservationDate = reservationDate;
+  
+  } //const with reservation info
+
+//to check if the reservation data is valid
+bool Reservation::hasValiData() const{
+  if (reservation <= 0){
+    return false;
+  }
+  if(studentId <= 0){
+    return false;
+  }
+  if (studentName.empty()){
+    return false;
+  }
+  if (resourceId <= 0){
+    return false;
+  }
+  if (reservationDate.empty()){
+    return false
+  }
+  return true; 
+} 
