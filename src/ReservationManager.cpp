fcab8af7caed57fe1e3bafe75cac60cc5408ff82
Ashish Thapa <asis.thapa121@gmail.com>
2026-09-15 10:12:20 -0500

    Update ReservationManager.cpp

--- src/ReservationManager.cpp
+++ src/ReservationManager.cpp
@@ -4,3 +4,33 @@
 
 using namespace std;
 
+ //default constructor
+ReservationManager::ReservationManager() {}
+
+//destructor
+ReservationManager::~ReservationManager() {} 
+
+//create/add reservation
+bool ReservationManager::addReservation(const Reservation& reservation) {}
+
+//cancel/remove reservation
+bool ReservationManager::cancelReservation(int reservationId) {} 
+
+//find reservation by ID
+Reservation* ReservationManager::findReservation(int reservationId) {}  
+
+//display active reservations
+void ReservationManager::displayActiveReservations() const {}
+
+//validate reservation before adding
+bool ReservationManager::validateReservation(const Reservation& reservation) const{} 
+
+//check if reservation ID already exists
+bool ReservationManager::reservationIdExists(int reservationId) const{} 
+
+//number of active reservations
+int ReservationManager::getActiveReservationCount() const{} 
+
+
+
+
