f7694f18c08625db61df67da8d17f61acfee7f34
utrejo18 <156870642+utrejo18@users.noreply.github.com>
2026-09-15 01:25:56 -0500

    Added WaitingList class and required structure for waiting list

--- include/WaitingList.h
+++ include/WaitingList.h
@@ -1,1 +1,18 @@
-// Waiting list queue
+//Waiting List using Queue
+#ifndef WAITING_LIST_H
+#define WAITING_LIST_H
+#include <queue>                //To enable queues
+#include "Reservation.h"        //Included Reservation.h since waiting list needs reservation info
+using namespace std;
+
+class WaitingList {
+private:
+    queue<Reservation> waitingList; //This creates a queue named waitingList that holds Reservation objects
+public:
+    void addStudent (const Reservation& reservation); //Adds student to the waiting list
+    bool removeStudent (Reservation& removedStudent); //Removes student from the waiting list, bool states if removal worked or not
+    void displayWaitingList() const; //Displays the waiting list
+};
+
+#endif
+
